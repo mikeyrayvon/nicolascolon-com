5bc4d75257f5a5f8249f9576dd75879638c90650
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-04-10 20:51:14 -0500

    add exhibition post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -3,49 +3,49 @@
 // https://developer.wordpress.org/resource/dashicons/
 function add_menu_icons_styles(){
 ?>
- 
+
 <style>
-#menu-posts-project .dashicons-admin-post:before {
+#menu-posts-exhibition .dashicons-admin-post:before {
     content: '\f319';
 }
 </style>
- 
+
 <?php
 }
 add_action( 'admin_head', 'add_menu_icons_styles' );
 
 
 //Register Custom Post Types
-add_action( 'init', 'register_cpt_project' );
+add_action( 'init', 'register_cpt_exhibition' );
 
-function register_cpt_project() {
+function register_cpt_exhibition() {
 
-    $labels = array( 
-        'name' => _x( 'Projects', 'project' ),
-        'singular_name' => _x( 'Project', 'project' ),
-        'add_new' => _x( 'Add New', 'project' ),
-        'add_new_item' => _x( 'Add New Project', 'project' ),
-        'edit_item' => _x( 'Edit Project', 'project' ),
-        'new_item' => _x( 'New Project', 'project' ),
-        'view_item' => _x( 'View Project', 'project' ),
-        'search_items' => _x( 'Search Projects', 'project' ),
-        'not_found' => _x( 'No projects found', 'project' ),
-        'not_found_in_trash' => _x( 'No projects found in Trash', 'project' ),
-        'parent_item_colon' => _x( 'Parent Project:', 'project' ),
-        'menu_name' => _x( 'Projects', 'project' ),
+    $labels = array(
+        'name' => _x( 'Exhibitions', 'exhibition' ),
+        'singular_name' => _x( 'Exhibition', 'exhibition' ),
+        'add_new' => _x( 'Add New', 'exhibition' ),
+        'add_new_item' => _x( 'Add New Exhibition', 'exhibition' ),
+        'edit_item' => _x( 'Edit Exhibition', 'exhibition' ),
+        'new_item' => _x( 'New Exhibition', 'exhibition' ),
+        'view_item' => _x( 'View Exhibition', 'exhibition' ),
+        'search_items' => _x( 'Search Exhibitions', 'exhibition' ),
+        'not_found' => _x( 'No exhibitions found', 'exhibition' ),
+        'not_found_in_trash' => _x( 'No exhibitions found in Trash', 'exhibition' ),
+        'parent_item_colon' => _x( 'Parent Exhibition:', 'exhibition' ),
+        'menu_name' => _x( 'Exhibitions', 'exhibition' ),
     );
 
-    $args = array( 
+    $args = array(
         'labels' => $labels,
         'hierarchical' => false,
-        
+
         'supports' => array( 'title', 'editor', 'thumbnail' ),
-        
+
         'public' => true,
         'show_ui' => true,
         'show_in_menu' => true,
         'menu_position' => 5,
-        
+
         'show_in_nav_menus' => true,
         'publicly_queryable' => true,
         'exclude_from_search' => false,
@@ -56,5 +56,5 @@ function register_cpt_project() {
         'capability_type' => 'post'
     );
 
-    register_post_type( 'project', $args );
+    register_post_type( 'exhibition', $args );
 }
